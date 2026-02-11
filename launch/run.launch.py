aba1534c99aeada8b2694bb680349a275c1099ff
Christopher Blum <cpb@ag-products.co.uk>
2026-02-11 12:22:26 +0000

    [CB]: Removed robot urdf (this is now stored in zapi_diff_control). Updated launch file to reflected this

--- launch/run.launch.py
+++ launch/run.launch.py
@@ -10,7 +10,8 @@ def generate_launch_description():
 
     share_dir = get_package_share_directory('lio_sam')
     parameter_file = LaunchConfiguration('params_file')
-    xacro_path = os.path.join(share_dir, 'config', 'robot.urdf.xacro')
+    diff_drive_dir = get_package_share_directory('zapi_diff_control')
+    xacro_path = os.path.join(diff_drive_dir, 'urdf', 'zapi_robot.urdf.xacro')
     rviz_config_file = os.path.join(share_dir, 'config', 'rviz2.rviz')
 
     params_declare = DeclareLaunchArgument(
